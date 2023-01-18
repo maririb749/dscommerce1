2d6676b50ac12bdb9a74d0efcd7ea100e246ccb7
maririb749 <maririb51@gmail.com>
2023-01-18 10:20:36 +0000

    Projeto criado

---
+++ backend/src/main/java/com/mariana/dscommerce1/Dscommerce1Application.java
@@ -0,0 +1,13 @@
+package com.mariana.dscommerce1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscommerce1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dscommerce1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mariana/dscommerce1/Dscommerce1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariana.dscommerce1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dscommerce1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
