7db9e37576f96d8bc586971b365e062de9c0ff80
maririb749 <maririb51@gmail.com>
2023-02-02 12:59:25 +0000

    Bean Validation

--- backend/src/main/java/com/mariana/dscommerce1/dto/ProductDTO.java
+++ backend/src/main/java/com/mariana/dscommerce1/dto/ProductDTO.java
@@ -1,12 +1,19 @@
 package com.mariana.dscommerce1.dto;
 
 import com.mariana.dscommerce1.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
     private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
-
+    @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço deve ser poditivo")
     private Double price;
     private String imgUrl;
 
