b419979b851fe85ba2ece31a13d5d001c3e7646b
maririb749 <maririb51@gmail.com>
2023-03-23 18:26:49 +0000

    Access control

--- backend/src/main/java/com/mariana/dscommerce1/config/ResourceServerConfig.java
+++ backend/src/main/java/com/mariana/dscommerce1/config/ResourceServerConfig.java
@@ -2,36 +2,45 @@ package com.mariana.dscommerce1.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-
 
 
     @Autowired
     private Environment env;
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
-        // H2 frames
         if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
             http.headers().frameOptions().disable();
         }
 
         http.authorizeRequests().anyRequest().permitAll();
 
-
     }
-
-
 }
 
