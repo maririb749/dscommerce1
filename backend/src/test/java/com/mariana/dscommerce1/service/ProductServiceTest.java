de97f368f27a76dfe1fe9deeec864e6e5e6c00b9
Mariana Santos <maririb51@gmail.com>
2023-09-28 21:05:11 +0100

    Teste busca de produto por id

---
+++ backend/src/test/java/com/mariana/dscommerce1/service/ProductServiceTest.java
@@ -0,0 +1,58 @@
+package com.mariana.dscommerce1.service;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.mariana.dscommerce1.dto.ProductDTO;
+import com.mariana.dscommerce1.entities.Product;
+import com.mariana.dscommerce1.repositories.ProductRepository;
+import com.mariana.dscommerce1.services.ProductService;
+import com.mariana.dscommerce1.tests.ProductFactory;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTest {
+
+	@InjectMocks
+	private ProductService service;
+
+	@Mock
+	private ProductRepository repository;
+
+	@SuppressWarnings("unused")
+	private long existingProductId, nonExistingProductId;
+	private String productName;
+	private Product product;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		existingProductId = 1L;
+		nonExistingProductId = 2L;
+
+		productName = "Playstation5";
+
+		product = ProductFactory.createProduct(productName);
+
+		Mockito.when(repository.findById(existingProductId)).thenReturn(Optional.of(product));
+
+	}
+
+	
+	@Test
+	public void findByIdShouldReturnProductDTOWhenIdExists() {
+		
+		ProductDTO result = service.findById(existingProductId);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getId(), existingProductId);
+		Assertions.assertEquals(result.getName(), product.getName());
+	}
+
+}
---
+++ backend/src/test/java/com/mariana/dscommerce1/tests/ProductFactory.java
@@ -0,0 +1,20 @@
+package com.mariana.dscommerce1.tests;
+
+import com.mariana.dscommerce1.entities.Category;
+import com.mariana.dscommerce1.entities.Product;
+
+public class ProductFactory {
+
+	public static Product createProduct() {
+		Category category = CategoryFactory.createCategory();
+		Product product = new Product(1L,"Console Playstation 5", "consectetur adipiscing elit,sed", 3999.0, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg");
+		product.getCategories().add(category);
+		return product;
+	}
+	
+	public static Product createProduct(String name) {
+		Product product = createProduct();
+		product.setName(name);
+		return product;
+	}
+}
